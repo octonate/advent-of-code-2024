ab27db5c076b77631d9721645d90412f5e0e0851
Nathan Goldberg <octonate9@gmail.com>
2024-12-07 02:07:36 -0500

    make template and helper function files

---
+++ template.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "helpers.c"
+
+int main(void) {
+    FILE *fp;
+    char line[LINE_LEN];
+    if ((fp = fopen("input.txt", "r")) == NULL) {
+        printf("cant open file bruh\n");
+        return 1;
+    }
+
+    while (fgets(line, sizeof(line), fp)) {
+    }
+
+    fclose(fp);
+    return 0;
+}

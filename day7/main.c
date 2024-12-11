18ca7e33526d4cc82f2010cfbea73570df61b0c2
Nathan Goldberg <octonate9@gmail.com>
2024-12-11 00:40:34 -0500

    slight optimization (still quite slow > 20s)

--- day7/main.c
+++ day7/main.c
@@ -13,20 +13,21 @@
     #define INPUT_TXT "input.txt"
     #define LIST_LEN 1024
 #endif
-/*
-        printf("  ");
-        printBitmask(bitmask, numsLen);
-        printf("\n& ");
-        printBitmask((uint16_t) 1 << i, numsLen);
-        printf("\n: ");
-        printf("%d\n\n", (bitmask & (1 << i)) > 0);
-*/
 
 void printBitmask(uint16_t bitmask, int numBits) {
     for (int i = 0; i < numBits; i++) {
         printf("%d", (bitmask & 1 << i) >> i);
     }
 }
+
+int64_t arrSum(int nums[], int numsLen) {
+    int64_t sum = 0;
+    for (int i = 0; i < numsLen; i++) {
+        sum += nums[i];
+    }
+    return sum;
+}
+
 
 int64_t evalWithBitmask(int nums[], int numsLen, uint16_t bitmask) {
     int64_t result = nums[0];
@@ -47,6 +48,50 @@ int64_t evalWithBitmask(int nums[], int numsLen, uint16_t bitmask) {
     return result;
 }
 
+int countDigits(int64_t num) {
+    return (int) floorl(log10l(num)) + 1;
+}
+
+int64_t int64Concat(int64_t num1, int64_t num2) {
+    //printf("counddigitsnum2: %d\n", countDigits(num2));
+    return num1 * (int64_t) powl(10.0l, countDigits(num2)) + num2;
+}
+
+int64_t arrConcat(int nums[], int numsLen) {
+    int64_t result = nums[0];
+    for (int i = 0; i < numsLen - 1; i++) {
+        result = int64Concat(result, nums[i + 1]);
+    }
+    return result;
+}
+
+int64_t evalWithBitmaskBase3(int nums[], int numsLen, uint32_t bitmask) {
+    int64_t result = nums[0];
+//    printf("%d", nums[0]);
+
+    for (int i = 0; i < numsLen - 1; i++) {
+        switch ((int)(bitmask / powl(3, i)) % 3) {
+        case 0:
+//            printf(" x ");
+            result *= nums[i + 1];
+            break;
+        case 1:
+//            printf(" + ");
+            result += nums[i + 1];
+            break;
+        case 2:
+//            printf(" || ");
+            result = int64Concat(result, nums[i + 1]);
+            break;
+        }
+//        printf("%d", nums[i + 1]);
+    }
+//    printf(" = %ld\n", result);
+    return result;
+}
+
+
+
 int main(void) {
     FILE *fp;
     char line[LINE_LEN];
@@ -56,7 +101,7 @@ int main(void) {
     }
 
     int64_t sum = 0;
-    int maxNumsLen = 0;
+    int64_t sum2 = 0;
 
     while (fgets(line, sizeof(line), fp)) {
         Str lineStr = strMake(line);
@@ -75,11 +120,8 @@ int main(void) {
             nums[numsLen] = strToI(tok);
             numsLen++;
         }
+        if (arrSum(nums, numsLen) > value) continue;
 
-    int64_t testSum = 1;
-    for (int i = 0; i < numsLen; i++) {
-        testSum *= nums[i];
-    }
         for (uint16_t i = 0; i < pow(2, numsLen - 1); i++) {
             if (evalWithBitmask(nums, numsLen, i) == value) {
                 sum += value;
@@ -87,12 +129,20 @@ int main(void) {
             }
         }
 
-        if (numsLen > maxNumsLen) {
-            maxNumsLen = numsLen;
+        uint32_t maxBitmaskBase3 = pow(3, numsLen - 1);
+        if (arrConcat(nums, numsLen) < value) continue;
+
+        for (uint32_t i = 0; i < maxBitmaskBase3; i++) {
+            int64_t eval = evalWithBitmaskBase3(nums, numsLen, i);
+            if (eval == value) {
+                sum2 += value;
+                break;
+            }
         }
     }
 
     printf("PART 1: %ld\n", sum);
+    printf("PART 2: %ld\n", sum2);
 
     fclose(fp);
     return 0;
